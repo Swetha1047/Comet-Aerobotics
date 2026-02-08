03db817045da16cbfd48c4299b82c6f9f83c78e0
Swetha1047 <swethaprakash174@gmail.com>
2026-02-08 15:03:08 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+Timport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/comet-aerobotics/',
   base: '/Comet-Aerobotics/', // MUST match repo name
 })
